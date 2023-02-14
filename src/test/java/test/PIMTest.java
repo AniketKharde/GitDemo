6554b6531fc545359eaadb523a6c79e7738acf75
Aniket <aniketkharde17@gmail.com>
2023-02-14 13:36:11 +0530

    New Change

--- src/test/java/test/PIMTest.java
+++ src/test/java/test/PIMTest.java
@@ -28,4 +28,14 @@ public class PIMTest extends CommonTest {
 		Assert.assertEquals(verifyEmpName, "lan");
 	}
 	
+	@Test
+	public void m2() {
+		System.out.println("M2");
+	}
+	
+	
+	public void m3() {
+		System.out.println("M3");
+	}
+	
 }
